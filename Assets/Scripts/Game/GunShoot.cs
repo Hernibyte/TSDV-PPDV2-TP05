88f24ac6302b927a3c0369bdf5f4b36932047f88
Gossa Hernan <hernanagustin.g24@hotmail.com>
2021-04-25 05:08:59 -0300

    Player controller & shoot system added.

---
+++ Assets/Scripts/Game/GunShoot.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunShoot : MonoBehaviour
+{
+    [SerializeField] float wait;
+    [SerializeField] Camera cam;
+    RaycastHit HitInfo;
+    float localTimer;
+
+    void Start()
+    {
+        localTimer = wait;
+    }
+
+    void Update()
+    {
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out HitInfo, 100.0f))
+        {
+            Debug.DrawRay(cam.transform.position, cam.transform.forward * 100.0f, Color.yellow);
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                localTimer += Time.deltaTime;
+                if (localTimer >= wait)
+                {
+                    localTimer = 0;
+                    Debug.Log(HitInfo);
+                }
+            }
+            else if (Input.GetKeyUp(KeyCode.Mouse0))
+                localTimer = wait;
+        }
+    }
+}
---
+++ Assets/Scripts/Game/MouseLook.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    [SerializeField] float mSensitivity = 100;
+    [SerializeField] Transform playerBody;
+    float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mSensitivity * Time.deltaTime;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
